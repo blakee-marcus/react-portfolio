98e289a262e3ab6e65de881271ab381f9c0aa5b0
bm <marcusb733@gmail.com>
2022-07-27 23:55:29 -0700

    Add Header to About Section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,10 +2,36 @@ import React from "react";
 
 function About() {
     return (
-        <section className="hero">
-            About Section
+        <section className='about'>
+            <div className='about-header'>
+                <div className='ah-left'>
+                    <div className='selfie'>
+                        <img src='/assets/images/self-illustration.png'></img>
+                    </div>
+                    <div className='about-title'>
+                        <div className='name'>Blake Marcus</div>
+                        <div className='headliner'>
+                            Full-Stack Web Developer
+                        </div>
+                    </div>
+                </div>
+                <div className="ah-right">
+                    <a className="social-btn github" href='https://github.com/blakee-marcus' target="_blank" rel="noopener noreferrer">
+                        <div className="btn-items"> 
+                            <img src='/assets/images/github-icon.png'/>
+                            <div>GitHub</div>
+                        </div>
+                    </a>
+                    <a className="social-btn linkedin" href='https://www.linkedin.com/in/blake-marcus/' target="_blank" rel="noopener noreferrer">
+                        <div className="btn-items">
+                        <img src='/assets/images/linkedin-icon.png'/>
+                            <div>LinkedIn</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </section>
-    )
+    );
 }
 
 export default About;
