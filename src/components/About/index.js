0e220c1301fe6d6222653d47307f013df42c447e
bm <marcusb733@gmail.com>
2022-07-31 07:36:31 -0700

    Add About Paragraphs

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,10 @@ function About() {
             <div className='about-header'>
                 <div className='ah-left'>
                     <div className='selfie'>
-                        <img src='/assets/images/self-illustration.png' alt="digital art of Blake"></img>
+                        <img
+                            src='/assets/images/self-illustration.png'
+                            alt='digital art of Blake'
+                        ></img>
                     </div>
                     <div className='about-title'>
                         <div className='name'>Blake Marcus</div>
@@ -23,7 +26,10 @@ function About() {
                         rel='noopener noreferrer'
                     >
                         <div className='btn-items'>
-                            <img src='/assets/images/github-icon.png' alt="github logo"/>
+                            <img
+                                src='/assets/images/github-icon.png'
+                                alt='github logo'
+                            />
                             <div>GitHub</div>
                         </div>
                     </a>
@@ -34,7 +40,10 @@ function About() {
                         rel='noopener noreferrer'
                     >
                         <div className='btn-items'>
-                            <img src='/assets/images/linkedin-icon.png' alt="linkedin logo"/>
+                            <img
+                                src='/assets/images/linkedin-icon.png'
+                                alt='linkedin logo'
+                            />
                             <div>LinkedIn</div>
                         </div>
                     </a>
@@ -55,9 +64,26 @@ function About() {
                 </ul>
             </div> */}
 
-            <div className="intro-container">
-                <div className="name"> About Me</div>
-                <p className="headliner"> Here's some text about myself wowie!!!</p>
+            <div className='intro-container'>
+                <div className='name intro'> About Me</div>
+                <p className='headliner intro-text'>
+                    {" "}
+                    Full-Stack Web Developer with a passion for creating
+                    mobile-first applications. Completed UCLA’s coding boot camp
+                    and earned a certificate in full-stack web development. My
+                    experience in management has allowed me to gain strengths in
+                    leadership, teamwork, and communication.
+                </p>
+                <p className='headliner intro-text'>
+                    {" "}
+                    One of my hobbies, aside from playing Apex, is working on
+                    streaming and stream production. I love helping friends make
+                    their stream look as pretty as possible. I'm always testing
+                    out new setups, settings, and art. Working on all of that
+                    has also allowed me to get proficient with Adobe Photoshop.
+                    It's also why you'll notice a heavy twitch influence on the
+                    design of my portfolio.
+                </p>
             </div>
         </section>
     );
