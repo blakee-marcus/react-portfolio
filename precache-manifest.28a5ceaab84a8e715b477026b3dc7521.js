2a2b1797ed3cee5c3278004c9e18a46db4e4628e
bm <marcusb733@gmail.com>
2022-07-31 07:42:06 -0700

    Updates

---
+++ precache-manifest.28a5ceaab84a8e715b477026b3dc7521.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "85848ead6406d8ec67d1",
+    "url": "/react-portfolio/static/css/main.468e63ab.chunk.css"
+  },
+  {
+    "revision": "85848ead6406d8ec67d1",
+    "url": "/react-portfolio/static/js/main.85848ead.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/react-portfolio/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "d867c84d6d0225191ac1",
+    "url": "/react-portfolio/static/js/2.d867c84d.chunk.js"
+  },
+  {
+    "revision": "47d92448d9d0b83fd7c0",
+    "url": "/react-portfolio/static/js/runtime~main.47d92448.js"
+  },
+  {
+    "revision": "b8e63f386222279e636f4d7ee548329b",
+    "url": "/react-portfolio/static/media/hamburger-icon.b8e63f38.svg"
+  },
+  {
+    "revision": "f59bcdf4e82c75bfc82cb72e0bf33982",
+    "url": "/react-portfolio/static/media/x-symbol-svgrepo-com.f59bcdf4.svg"
+  },
+  {
+    "revision": "0d8bf95835879829a006a4bc3500f276",
+    "url": "/react-portfolio/index.html"
+  }
+];
