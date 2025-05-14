dfe5396e598f506e62d324f36e3440fc15e0e638
blakee-marcus <marcusb733@gmail.com>
2025-05-14 10:33:37 -0700

    Fix nav bar on mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <nav className='bg-white text-black border-b-8 border-black font-mono'>
-      <ul className='flex justify-center text-lg uppercase tracking-wider'>
+      <ul className='flex flex-col sm:flex-row justify-center text-lg uppercase tracking-wider'>
         {[
           { to: '/', label: 'Home' },
           { to: '/about', label: 'About' },
           { to: '/projects', label: 'Projects' },
           { to: '/contact', label: 'Contact' },
-        ].map(({ to, label }, i) => (
+        ].map(({ to, label }) => (
           <li
             key={label}
-            className={`px-8 py-6 border-l-4 border-black first:border-l-0 hover:bg-black hover:text-white transition-colors`}>
+            className='px-8 py-6 border-t sm:border-t-0 sm:border-l-4 border-black first:sm:border-l-0 hover:bg-black hover:text-white transition-colors'>
             <Link to={to} className='font-bold'>
               {label}
             </Link>
