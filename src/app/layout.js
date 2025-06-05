8f67a967db5fbb2ba44699bee96449925e7f1db5
blakee-marcus <marcusb733@gmail.com>
2025-06-05 01:48:55 -0700

    feat: add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
@@ -12,7 +14,10 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body>
         <NavBar />
-        <main className='min-h-screen pb-3'>{children}</main>
+        <main className='min-h-screen pb-3'>{children}
+          <Analytics />
+          <SpeedInsights />
+        </main>
         <Footer />
       </body>
     </html>
